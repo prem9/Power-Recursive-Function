e2e08296f0fccc34cf03c032edc1e8e006771a39
Prem Agarwal <prem.m.agarwal@gmail.com>
2022-04-26 20:26:05 -0700

    implementing power recursive function

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,35 @@
 package com.example;
-
+import java.util.Scanner;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args) 
     {
-        System.out.println( "Hello World!" );
-    }
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter an integer for the base: ");
+        int base = in.nextInt();
+        System.out.println("");
+        System.out.print("Enter an integer for the exponent: ");
+        int powerRaised = in.nextInt();
+        System.out.println("");
+        int result = power(base, powerRaised);
+    
+        System.out.println(base + "^" + powerRaised + "=" + result);
+        }
+      public static int power(int base, int powerRaised) 
+      {
+        if (powerRaised != 0) 
+        {
+    
+          // recursive call to power()
+          return (base * power(base, powerRaised - 1));
+        }
+        else 
+        {
+          return 1;
+        }
+      }
 }
